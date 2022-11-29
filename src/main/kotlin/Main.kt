682948e25d173e9a77535716d7374488f97be712
Josh <joshua.cook1203@gmail.com>
2022-11-29 15:04:34 +1300

    test

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,7 @@
 fun main(args: Array<String>) {
     println("Hello World!")
+    var ecksdee = "14"
+    print(ecksdee)
 
     // Try adding program arguments via Run/Debug configuration.
     // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
